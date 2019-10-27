210939fff6eccb920754a0feaafc7f6ec5ba6c97
wallacemkk <wallaceedua@gmail.com>
2019-10-27 16:34:56 -0300

    start new repo

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+	Os códigos em Go são organizados em pacotes
+	e para usá-los é necessário declarar em um ou vários pacotes
+*/
+import "fmt"
+
+// A porta de entrada de um programa em Go é a função main
+func main() {
+	fmt.Println("Primeiro")
+	fmt.Println("Programa")
+}
